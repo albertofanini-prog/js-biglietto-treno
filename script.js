9a95f59e597880e95b2c50551f9d45e04b3a0c49
albertofanini-prog <albertofanini@outlook.com>
2022-02-04 16:10:02 +0100

    3rd Commit - Calcolatore funzionante con prezzo base e sconti applicati

--- script.js
+++ script.js
@@ -20,18 +20,28 @@ console.log(age)
 
 //Calcolare il prezzo
 const price = km * 0.21;
-console.log(price)
+console.log(price);
 
 
 //Calcolare il prezzo per under 18 con -20%
-const priceU18 = price - ((20*price) / 100)
-console.log(priceU18)
+const priceU18 = price - ((20*price) / 100);
+console.log(priceU18);
 
 
 //Calcolare il prezzo per over 65 con -40%
-const priceO65 = price - ((40*price) / 100)
-console.log(priceO65)
+const priceO65 = price - ((40*price) / 100);
+console.log(priceO65);
 
 
-//const datiHTML = document.getElementById('price')
-//datiHTML.innerHTML = `Il prezzo del suo biglietto sarà di `
+//Dichiarare il prezzo
+const datiHTML = document.getElementById('price');
+datiHTML.innerHTML = `Il prezzo del suo biglietto sarà di ${price}€`;
+
+    if(age < 18){
+        datiHTML.innerHTML = `Il prezzo del suo biglietto sarà di ${priceU18}`;
+    }
+
+    else if(age > 65){
+        datiHTML.innerHTML = `Il prezzo del suo biglietto sarà di ${priceO65}`;
+    }
+    
