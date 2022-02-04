c9acf31a75fa59881ad70aee64db6a07ed50e0c1
albertofanini-prog <albertofanini@outlook.com>
2022-02-04 15:40:31 +0100

    2nd Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//Chiedere il numero di km da percorrere
+//Chiedere l'età
+//Il prezzo è di 0.21€ al km
+//Sconto di 20% per <18
+//Sconto di 40% per >65
+
+
+//Chiedere il numero di km da percorrere
+const km = prompt('Benvenuto in BoolTrain. Quanti km dovrai percorrere?');
+console.log(km);
+
+
+//Chiedere l'età
+const age = prompt('Inserisci la tua età, perfavore.');
+console.log(age)
+
+
+//alert('Il prezzo è di 0.21€ per km percorso. Gli under 18 hanno diritto a uno sconto del 20%, gli over 65 hanno diritto a uno sconto del 40%.')
+
+
+//Calcolare il prezzo
+const price = km * 0.21;
+console.log(price)
+
+
+//Calcolare il prezzo per under 18 con -20%
+const priceU18 = price - ((20*price) / 100)
+console.log(priceU18)
+
+
+//Calcolare il prezzo per over 65 con -40%
+const priceO65 = price - ((40*price) / 100)
+console.log(priceO65)
+
+
+//const datiHTML = document.getElementById('price')
+//datiHTML.innerHTML = `Il prezzo del suo biglietto sarà di `
